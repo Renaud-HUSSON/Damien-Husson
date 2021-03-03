329e5f241d86e00dd68f28f88aa5c7a7d3350dfc
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-03-03 22:11:53 +0100

    feat(Api): Add static images folder

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,25 +1,39 @@
+import { join } from 'path'
+import AutoLoad, { AutoloadPluginOptions } from 'fastify-autoload'
+import { FastifyPluginAsync } from 'fastify'
+import db from './config/Database'
 import { join } from 'path';
 import AutoLoad, {AutoloadPluginOptions} from 'fastify-autoload';
 import { FastifyPluginAsync } from 'fastify';
+import fastifyStatic from 'fastify-static'
+import * as path from 'path'
+
+db.sequelize.sync()
 
 export type AppOptions = {
 } & Partial<AutoloadPluginOptions>;
 
+export type AppOptions = {} & Partial<AutoloadPluginOptions>
+
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+  fastify,
+  opts
 ): Promise<void> => {
+  fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'images'),
+    prefix: '/images/',
+  })
 
-  void fastify.register(AutoLoad, {
+  fastify.register(AutoLoad, {
     dir: join(__dirname, 'plugins'),
-    options: opts
+    options: opts,
   })
 
-  void fastify.register(AutoLoad, {
+  fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
-    options: opts
+    options: opts,
   })
-};
+}
 
-export default app;
+export default app
 export { app }
