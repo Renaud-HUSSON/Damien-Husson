d239b23ae1391f48f51c3154f80100fde8c2a266
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-03-04 18:47:00 +0100

    feat(Api): Model & Attributes interfaces for sequelize

--- api/src/ts/interfaces/db.ts
+++ api/src/ts/interfaces/db.ts
@@ -1,7 +1,36 @@
-import { ModelCtor, Sequelize } from 'sequelize'
+import { Model, ModelCtor, Optional, Sequelize } from 'sequelize'
+
+export interface RealisationAttributes {
+  id: number
+  titre: string
+  image: string
+}
+
+export interface RealisationCreationAttributes
+  extends Optional<RealisationAttributes, 'id'> {}
+
+export interface Realisation
+  extends Model<RealisationAttributes, RealisationCreationAttributes> {
+  dataValues: RealisationAttributes
+}
+
+export interface CompetencesAttributes {
+  id: number
+  titre: string
+  image: string
+  description: string
+}
+
+export interface CompetenceCreationAttributes
+  extends Optional<CompetencesAttributes, 'id'> {}
+
+export interface Competence
+  extends Model<CompetencesAttributes, CompetenceCreationAttributes> {
+  dataValues: CompetencesAttributes
+}
 
 export interface Database {
   sequelize: Sequelize
-  realisations: ModelCtor<any>
-  competences?: ModelCtor<any>
+  realisations: ModelCtor<Realisation>
+  competences: ModelCtor<Competence>
 }
