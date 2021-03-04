329fd8eb242b7ad13a74001bd664e3f7717cbf29
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-03-04 18:47:46 +0100

    feat(Api): Sequelize's Realisations model

---
+++ api/src/models/RealisationModel.ts
@@ -0,0 +1,27 @@
+import { ModelCtor, Sequelize } from 'sequelize/types'
+import { DataTypes } from 'sequelize'
+import { Realisation } from '../ts/interfaces/db'
+
+export default (sequelize: Sequelize): ModelCtor<Realisation> => {
+  return sequelize.define<Realisation>(
+    'Realisations',
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      titre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'realisations',
+    }
+  )
+}
