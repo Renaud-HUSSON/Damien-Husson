7aeb5fc381c30fb78eb027f5a835996858117a66
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-03-09 21:40:50 +0100

    refactor(Api): rename get_error_response to error_response

--- api/src/@types/error_response.d.ts
+++ api/src/@types/error_response.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export interface GetErrorResponseSchemaInterface {
+export interface ErrorResponseSchemaInterface {
   statusCode?: number;
   error?: string;
   message?: string;
