e5ed96f70d2840739210689eae4b902ee7262311
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-03-11 17:00:39 +0100

    build(Client): Remove tailwind from the project

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,4 @@
 import { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
-import '../styles/styles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
