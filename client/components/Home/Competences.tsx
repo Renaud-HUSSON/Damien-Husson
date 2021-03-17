b0b991eb66247c932132b1b6b3382a00f0b959b1
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-03-17 13:48:16 +0100

    fix(Client): Perfecting skills & presentation appear

--- client/components/Home/Competences.tsx
+++ client/components/Home/Competences.tsx
@@ -16,23 +16,20 @@ export const Competences = ({ competences }: CompetencesProps) => {
   return (
     <>
       <div id='competences'></div>
-      <section
-        className={`home__competences ${
-          inView && 'home__competences__visible'
-        }`}
-        ref={ref}
-      >
-        <h2>Mes Compétences</h2>
-        <div className='home__competences__grid'>
-          {competences.map((competence) => {
-            return (
-              <div key={competence.id}>
-                <img src={competence.image} alt={competence.titre} />
-                <h3>{competence.titre}</h3>
-                <p>{competence.description}</p>
-              </div>
-            )
-          })}
+      <section className='home__competences' ref={ref}>
+        <div ref={ref} className={`${inView && 'home__competences__visible'}`}>
+          <h2>Mes Compétences</h2>
+          <div className='home__competences__grid'>
+            {competences.map((competence) => {
+              return (
+                <div key={competence.id}>
+                  <img src={competence.image} alt={competence.titre} />
+                  <h3>{competence.titre}</h3>
+                  <p>{competence.description}</p>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </section>
     </>
--- client/components/Home/Presentation.tsx
+++ client/components/Home/Presentation.tsx
@@ -3,43 +3,42 @@ import { useInView } from 'react-intersection-observer'
 export const Presentation = () => {
   const options = {
     triggerOnce: true,
-    threshold: 0.1,
   }
 
   const { ref, inView } = useInView(options)
 
+  console.log(inView)
+
   return (
     <>
       <div id='presentation'></div>
-      <section
-        className={`home__presentation ${
-          inView && 'home__presentation__visible'
-        }`}
-        ref={ref}
-      >
-        <h2>Présentation</h2>
-        <div>
-          <p>
-            Je m'appelle Damien, j'ai 18 ans, je suis originaire de Auch dans le
-            Gers mais je fais mes études à Toulon. Je suis actuellement en DUT
-            MMI mais j'aimerais me réorienter vers un DN MADE mention graphisme
-            à Toulouse.{' '}
-          </p>
-          <p>
-            Grâce à cette première année d'étude supérieure, j'ai appris
-            beaucoup de chose mais j'ai surtout découvert une passion, le
-            graphisme.{' '}
-          </p>
-          <p>
-            Aujourd'hui je me définis comme quelqu'un de créatif, autonome et
-            curieux. J'adore découvrir de nouvelle choses, c'est pour ça que je
-            me forme par moi même dans différents domaines en dehors des études
-          </p>
-          <p>
-            Vous découvrirez sur mon portfolio toutes mes créations que ce soit
-            des dessins vectoriels, des photomontages, du web design ou encore
-            des illustrations
-          </p>
+      <section className='home__presentation'>
+        <div ref={ref} className={`${inView && 'home__presentation__visible'}`}>
+          <h2>Présentation</h2>
+          <div>
+            <p>
+              Je m'appelle Damien, j'ai 18 ans, je suis originaire de Auch dans
+              le Gers mais je fais mes études à Toulon. Je suis actuellement en
+              DUT MMI mais j'aimerais me réorienter vers un DN MADE mention
+              graphisme à Toulouse.{' '}
+            </p>
+            <p>
+              Grâce à cette première année d'étude supérieure, j'ai appris
+              beaucoup de chose mais j'ai surtout découvert une passion, le
+              graphisme.{' '}
+            </p>
+            <p>
+              Aujourd'hui je me définis comme quelqu'un de créatif, autonome et
+              curieux. J'adore découvrir de nouvelle choses, c'est pour ça que
+              je me forme par moi même dans différents domaines en dehors des
+              études
+            </p>
+            <p>
+              Vous découvrirez sur mon portfolio toutes mes créations que ce
+              soit des dessins vectoriels, des photomontages, du web design ou
+              encore des illustrations
+            </p>
+          </div>
         </div>
       </section>
     </>
