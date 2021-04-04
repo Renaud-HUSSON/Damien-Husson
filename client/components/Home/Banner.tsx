549139d03216cd14b855a584af2d67d245cd908a
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-04-04 17:58:48 +0200

    feat(Banner): Animate landing page

--- client/components/Home/Banner.tsx
+++ client/components/Home/Banner.tsx
@@ -5,25 +5,30 @@ export const Banner = () => {
         <div className='home__banner__line_color'></div>
       </div>
       <p>
-        Bonjour, je suis un étudiant passionné par le graphisme. <br />
-        Bienvenue sur mon portfolio !
+        <span>Bonjour, je suis un étudiant passionné par le graphisme.</span>{' '}
+        <br />
+        <span>Bienvenue sur mon portfolio !</span>
       </p>
       <h1>
         <div>
-          <span>D</span>
-          <span>a</span>
-          <span>m</span>
-          <span>i</span>
-          <span>e</span>
-          <span>n</span>
+          <div>
+            <span>D</span>
+            <span>a</span>
+            <span>m</span>
+            <span>i</span>
+            <span>e</span>
+            <span>n</span>
+          </div>
         </div>
         <div>
-          <span>H</span>
-          <span>u</span>
-          <span>s</span>
-          <span>s</span>
-          <span>o</span>
-          <span>n</span>
+          <div>
+            <span>H</span>
+            <span>u</span>
+            <span>s</span>
+            <span>s</span>
+            <span>o</span>
+            <span>n</span>
+          </div>
         </div>
       </h1>
       <div className='home__banner_scroll'>
