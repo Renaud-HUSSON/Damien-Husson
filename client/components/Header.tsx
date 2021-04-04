39dc3c86e7c6f5b054a3e5ab145f63518f1117ec
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-04-04 17:58:24 +0200

    feat(Header): Make it sticky when scrolling down

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { useInView } from 'react-intersection-observer'
 
 export const Header = () => {
   const router = useRouter()
   const [navOpened, setNavOpened] = useState(false)
+
+  const options = {
+    threshold: 0,
+  }
+
+  const { ref, inView } = useInView(options)
 
   const handleClick = () => {
     setNavOpened((nav) => !nav)
@@ -22,44 +29,46 @@ export const Header = () => {
   }, [router])
 
   return (
-    <header>
-      <Link href='/'>
-        <a>
-          <img src='/assets/logo.png' alt='Logo' />
-        </a>
-      </Link>
-      <ul className={navOpened ? 'opened' : ''}>
-        <Link href='#presentation'>
+    <div className='header__wrapper' ref={ref}>
+      <header className={!inView ? 'header--sticky' : ''}>
+        <Link href='/'>
           <a>
-            <li>PRÉSENTATION</li>
+            <img src='/assets/logo.png' alt='Logo' />
           </a>
         </Link>
-        <Link href='#references'>
-          <a>
-            <li>RÉFÉRENCES</li>
-          </a>
-        </Link>
-        <Link href='#competences'>
-          <a>
-            <li>COMPÉTENCES</li>
-          </a>
-        </Link>
-        <Link href='#realisations'>
-          <a>
-            <li>RÉALISATIONS</li>
-          </a>
-        </Link>
-        <Link href='#contact'>
-          <a>
-            <li>CONTACT</li>
-          </a>
-        </Link>
-      </ul>
-      <div onClick={handleClick}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </header>
+        <ul className={navOpened ? 'opened' : ''}>
+          <Link href='#presentation'>
+            <a>
+              <li>PRÉSENTATION</li>
+            </a>
+          </Link>
+          <Link href='#references'>
+            <a>
+              <li>RÉFÉRENCES</li>
+            </a>
+          </Link>
+          <Link href='#competences'>
+            <a>
+              <li>COMPÉTENCES</li>
+            </a>
+          </Link>
+          <Link href='#realisations'>
+            <a>
+              <li>RÉALISATIONS</li>
+            </a>
+          </Link>
+          <Link href='#contact'>
+            <a>
+              <li>CONTACT</li>
+            </a>
+          </Link>
+        </ul>
+        <div className={navOpened ? 'opened' : ''} onClick={handleClick}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </header>
+    </div>
   )
 }
